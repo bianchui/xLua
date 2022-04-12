26f53c2ea8df1e88cdf1e78a42a742301e68b129
bianchui <bianchui@gmail.com>
2022-04-12 18:57:11 +0800

    build xlua with EmmyDebugger

--- build/make_osx_luajit.sh
+++ build/make_osx_luajit.sh
@@ -1,5 +1,5 @@
 mkdir -p build_lj_osx && cd build_lj_osx
-cmake -DUSING_LUAJIT=ON  -GXcode ../
+cmake -DCMAKE_CXX_STANDARD=11 -DUSING_LUAJIT=ON -DEMMY_LUA_VERSION=jit -GXcode ../
 cd ..
 cmake --build build_lj_osx --config Release
 mkdir -p plugin_luajit/Plugins/xlua.bundle/Contents/MacOS/
